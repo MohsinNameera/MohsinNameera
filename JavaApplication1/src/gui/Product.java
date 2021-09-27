22a1609ad2e86d8e6bdcad3c25e7e679ff2a39e1
Nameera Mohsin <mohsin.n@northeastern.edu>
2021-09-26 22:36:22 -0400

    adding my 1st AED assignment

---
+++ JavaApplication1/src/gui/Product.java
@@ -0,0 +1,191 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author namir
+ */
+public class Product {
+    
+
+    private String name;
+    private String address;
+    private String dob;
+    private String contactNo;
+    private String email;
+    private String ssn;
+    private String medicalRecordNo;
+    private String healthPolicyNo;
+    private String bankAccountNo;
+    private String licenseNo;
+    private String vehicleNo;
+    private String deviceNo;
+    private String linkedIn;
+    private String IPAddress;
+    private ImageIcon labelPhoto1;
+    private String jEditorPane2;
+    
+    
+/*public Product( String name, String address, String dob, String contactNo, String email, String ssn, String medicalRecordNo, String healthPolicyNo, String bankAccountNo, String licenseNo, String vehicleNo, String deviceNo, String linkedIn, String IPAddress, ImageIcon labelPhoto1, String jEditorPane2)
+{
+       
+       this.name = name;
+       this.address  = address;
+       this.dob = dob;
+       this.contactNo = contactNo;
+       this.email= email;
+       this.ssn= ssn;
+       this.medicalRecordNo= medicalRecordNo;
+       this.healthPolicyNo = healthPolicyNo;
+       this.bankAccountNo= bankAccountNo;
+       this.licenseNo= licenseNo;
+       this.vehicleNo= vehicleNo;
+       this.deviceNo= deviceNo;
+       this.linkedIn= linkedIn;
+       this.IPAddress= IPAddress;
+       this.labelPhoto1= labelPhoto1;
+       this.jEditorPane2= jEditorPane2;
+       
+}*/
+
+    public ImageIcon getLabelPhoto1() {
+        return labelPhoto1;
+    }
+
+    public void setLabelPhoto1(ImageIcon labelPhoto1) {
+        this.labelPhoto1 = labelPhoto1;
+    }
+
+    public String getjEditorPane2() {
+        return jEditorPane2;
+    }
+
+    public void setjEditorPane2(String jEditorPane2) {
+        this.jEditorPane2 = jEditorPane2;
+    }
+   
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalRecordNo() {
+        return medicalRecordNo;
+    }
+
+    public void setMedicalRecordNo(String medicalRecordNo) {
+        this.medicalRecordNo = medicalRecordNo;
+    }
+
+    public String getHealthPolicyNo() {
+        return healthPolicyNo;
+    }
+
+    public void setHealthPolicyNo(String healthPolicyNo) {
+        this.healthPolicyNo = healthPolicyNo;
+    }
+
+    public String getBankAccountNo() {
+        return bankAccountNo;
+    }
+
+    public void setBankAccountNo(String bankAccountNo) {
+        this.bankAccountNo = bankAccountNo;
+    }
+
+    public String getLicenseNo() {
+        return licenseNo;
+    }
+
+    public void setLicenseNo(String licenseNo) {
+        this.licenseNo = licenseNo;
+    }
+
+    public String getVehicleNo() {
+        return vehicleNo;
+    }
+
+    public void setVehicleNo(String vehicleNo) {
+        this.vehicleNo = vehicleNo;
+    }
+
+    public String getDeviceNo() {
+        return deviceNo;
+    }
+
+    public void setDeviceNo(String deviceNo) {
+        this.deviceNo = deviceNo;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIPAddress() {
+        return IPAddress;
+    }
+
+    public void setIPAddress(String IPAddress) {
+        this.IPAddress = IPAddress;
+    }
+
+   
+}
+
+    
+
+
+   
